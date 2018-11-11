d8b57d31ed9fb802a0a520dd4d44053931a26818
rahul1892 <kumar.rahul1892@gmail.com>
2018-11-11 05:30:15 +0000

    Makefile: Remove pathmap NOS doesn't follow pathmap and hence HALs were not building. This fixes it. This commit should be reverted if building other rom which support pathmaps.

--- Android.mk
+++ Android.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(call my-dir),$(call project-path-for,qcom-thermal))
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -44,5 +42,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
